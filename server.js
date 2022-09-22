9d6a1b57e10d6752c8fa6067cc8403079dcba0df
Ryan Ott <rott9501@gmail.com>
2022-09-21 20:49:31 -0700

    rollbar initial commit

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '1a2991737a874f958e3ed93144beccd3',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 
 app.get('/api/robots', (req, res) => {
     try {
