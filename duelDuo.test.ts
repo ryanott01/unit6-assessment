d916152f276c110475bf45e667cee414b27fe693
Ryan Ott <rott9501@gmail.com>
2022-09-21 19:49:05 -0700

    initial commit

---
+++ duelDuo.test.ts
@@ -0,0 +1,45 @@
+
+import { Builder, Capabilities, By } from "selenium-webdriver"
+import { Executor } from "selenium-webdriver/http"
+import { titleContains } from "selenium-webdriver/lib/until"
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeEach(async () => {
+    driver.get('http://127.0.0.1:5500/public/')
+})
+
+afterAll(async () => {
+    driver.quit()
+})
+
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+    await driver.sleep(2000);
+})
+
+
+test('clicking draw button displays choices', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    await draw.click();
+    await driver.sleep(500)
+    const choices = await driver.findElement(By.id('choices'))
+    const displayed = await choices.isDisplayed()
+    expect(displayed).toBe(true)
+    await driver.sleep(2000);
+})
+
+test('clicking', async () => {
+    const draw = await driver.findElement(By.id('draw'))
+    await draw.click();
+    const button = await driver.findElement(By.className('bot-btn'))
+    await button.click()
+    const playerDuo = await driver.findElement(By.id('player-duo'))
+    const displayed = await playerDuo.isDisplayed()
+    expect(displayed).toBe(true)
+
+})
---
+++ functions.test.ts
@@ -0,0 +1,25 @@
+// const {shuffleArray} = require('./utils')
+
+// let testArr = ['test','this','array']
+
+// describe('shuffleArray should', () => {
+//    const shuffledArray = shuffleArray(testArr)
+//     test('is an array', () => {
+//         expect(shuffledArray).toBe('array')
+//     })
+   
+//     test('test array length', () => {
+//         expect(shuffledArray.length).toEqual(testArr.length)
+//     })
+
+//     test('all items are in array',() =>{
+//         expect(shuffledArray).toContain(testArr)
+//     })
+
+//     test('items have been shuffled',() => {
+//         expect(shuffledArray).toContain(testArr)
+//         expect(shuffledArray).not.toEqual(testArr)
+//     })
+        
+// })
+
